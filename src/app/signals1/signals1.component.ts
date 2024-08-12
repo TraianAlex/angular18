7c7415ac1c6101cd9d3e5629be7059394e9186ab
Traian Alexandru <victor_traian@yahoo.com>
2024-08-11 22:16:33 -0400

    s1

--- src/app/signals1/signals1.component.ts
+++ src/app/signals1/signals1.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, OnInit, signal } from '@angular/core';
+
+type OrderStatus = 'placed' | 'cooking' | 'delivered';
+type UserData = {
+  id: number;
+  name: string;
+  email: string;
+};
 
 @Component({
   selector: 'app-signals1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './signals1.component.html',
   styleUrl: './signals1.component.scss',
 })
-export class Signals1Component {}
+export class Signals1Component implements OnInit {
+  mySignal = signal({ foo: 'bar' });
+  count = signal<number>(1);
+  orderStatus = signal<OrderStatus>('placed');
+  userLoggedIn = signal<boolean>(false);
+  cartItemsSignal = signal<string[]>([]);
+  myObject = signal(
+    {
+      name: 'Nnamdi',
+    },
+    {
+      equal: (a: { name: any }, b: { name: any }) => a.name === b.name,
+    }
+  );
+
+  ngOnInit(): void {
+    // Update order status triggers recomputation of prepareFoodValue
+    this.orderStatus.set('cooking');
+
+    // Simulate user login (you can replace this with actual login logic)
+    this.userLoggedIn.set(true);
+    // When user logs in, add dependency on user data (simulated by userDataSignal)
+    computed(() => {
+      if (this.userLoggedIn()) {
+        // Add dependency
+      } else {
+        // Remove dependency }
+      }
+    });
+    this.cartItemsSignal.update((items) => [...this.cartItemsSignal(), 'bread']);
+  }
+
+  setNewValue() {
+    this.mySignal.set({ foo: 'bar1' });
+  }
+
+  updateValue() {
+    const currentValue = this.mySignal();
+    this.mySignal.set({ ...currentValue, foo: currentValue.foo + '1' });
+  }
+
+  doubleCountValue = computed(() => {
+    return this.count() * 2;
+  });
+
+  // Create a computed signal (derived from order status) for food preparation
+  prepareFoodValue = computed(() => {
+    return this.orderStatus() === 'placed' ? 'preparing' : 'idle';
+  });
+
+  showProfileValue = computed(() => {
+    return this.userLoggedIn() ? 'Yes' : 'No';
+  });
+
+  areObjectsEqual = computed(() => {
+    return this.myObject() === this.mySignal;
+  });
+}

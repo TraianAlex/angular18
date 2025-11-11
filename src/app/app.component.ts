7a015487c0a40cdfc11293e33f44aa07f1307754
Traian Alexandru <victor_traian@yahoo.com>
2025-11-11 16:35:01 -0500

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, RouterLink],
+    imports: [RouterOutlet, NavigationComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })

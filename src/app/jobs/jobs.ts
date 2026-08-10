6d12f0572d4ff6ff03c954074794f19c12374a89
Traian Alexandru <victor_traian@yahoo.com>
2026-08-10 12:21:02 -0400

    add class active when user navigate to details

--- src/app/jobs/jobs.ts
+++ src/app/jobs/jobs.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { filter, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-jobs',
@@ -7,4 +9,22 @@ import { RouterModule } from '@angular/router';
   templateUrl: './jobs.html',
   styleUrl: './jobs.scss',
 })
-export class JobsComponent {}
+export class JobsComponent {
+  private readonly router = inject(Router);
+
+  private readonly url = toSignal(
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      map(() => this.router.url),
+      startWith(this.router.url),
+    ),
+    { initialValue: this.router.url },
+  );
+
+  readonly jobsTabActive = computed(() => {
+    const path = this.url().split('?')[0];
+    return path === '/jobs' || path.startsWith('/jobs/job');
+  });
+
+  readonly favoritesTabActive = computed(() => this.url().split('?')[0].startsWith('/jobs/favorites'));
+}

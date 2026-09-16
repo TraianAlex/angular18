a442531d104ca60625e9d21f7ce76be1b824b70f
Traian Alexandru <victor_traian@yahoo.com>
2026-09-16 17:22:05 -0400

    ref user info form

---
+++ src/app/test/playground/user-info.ts
@@ -0,0 +1,171 @@
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  form,
+  FormRoot,
+  FormField,
+  submit,
+  required,
+  maxLength,
+  minLength,
+  pattern,
+  apply,
+  schema,
+  applyWhen,
+  applyWhenValue,
+  min,
+} from '@angular/forms/signals';
+import { JsonPipe } from '@angular/common';
+import { UserService } from './user.service';
+import { mustBeFromValidProvider } from './validators/cc-validator';
+import { validateCreditCardNumber } from './validators/credit-card-validator';
+import { Address, addressSchema } from './validators/address-schema-validation';
+import { registerZipValidation } from './validators/zip.validator';
+import { StarRatingComponent } from './star-rating/star-rating';
+
+interface UsAddress {
+  country: 'US';
+  street: string;
+  city: string;
+  zip: string;
+}
+interface CaAddress {
+  country: 'CA';
+  street: string;
+  city: string;
+  zip: string;
+}
+type Address2 = UsAddress | CaAddress;
+
+const usZipSchema = schema<Address>((address) => {
+  pattern(address.zip, /^\d{5}$/, { message: 'Zip code must be 5 digits' });
+});
+const caZipSchema = schema<Address>((address) => {
+  pattern(
+    address.zip,
+    /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ] \d[ABCEGHJKLMNPRSTVWXYZ]\d$/,
+    {
+      message: 'Zip code must follow the A1A 1A1 format',
+    },
+  );
+});
+
+@Component({
+  selector: 'app-user-info',
+  templateUrl: './user-info.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [FormRoot, FormField, JsonPipe, StarRatingComponent],
+})
+export class UserInfoComponent {
+  userService = inject(UserService);
+
+  constructor() {
+    this.userForm.address.city().value.set('John');
+    console.log(this.userForm.address.street().invalid(), this.userForm.address().valid());
+  }
+
+  INITIAL_USER_INFO = {
+    firstName: '',
+    lastName: '',
+    address: {
+      street: '',
+      country: '',
+      city: '',
+      zip: '',
+    },
+    cc: '',
+    rating: 0,
+  };
+  userInfo = signal({ ...this.INITIAL_USER_INFO });
+
+  userForm = form(
+    this.userInfo,
+    (path) => {
+      required(path.firstName, { message: 'First name is required' });
+      required(path.lastName, { message: 'Last name is required' });
+      // required(path.cc, { message: 'Credit card number is required' });
+      // minLength(path.cc, 16, { message: 'Credit card number must have 16 digits' });
+      // maxLength(path.cc, 16, { message: 'Credit card number must be less than 17 digits' });
+      // mustBeFromValidProvider(path.cc);
+      validateCreditCardNumber(path.cc);
+      // required(path.address.street, { message: 'Address is required' });
+      // required(path.address.country, { message: 'Country is required' });
+      // required(path.address.city, { message: 'City is required' });
+      // required(path.address.zip, { message: 'Zip is required' });
+      apply(path.address, addressSchema);
+      // pattern(path.address.zip, /^\d{5}$/, { message: 'Zip must be 5 digits' });
+      // pattern(path.address.zip, /^\d{5}$/, {
+      //   message: 'Zip code must be 5 digits',
+      //   when: ({ valueOf }) => valueOf(path.address.country) === 'US',
+      // });
+      // pattern(path.address.zip, / ([ABCEGHJKLMNPRSTVXY] \d)([ABCEGHJKLMNPRSTVWXYZ] \d) {2}/, {
+      //   message: 'Zip code must follow the A1A 1A1 format',
+      //   when: ({ valueOf }) => valueOf(path.address.country) === 'CA',
+      // });
+      // applyWhen(
+      //   path.address,
+      //   ({ valueOf }) => valueOf(path.address.country) === 'US',
+      //   usZipSchema,
+      // );
+      // applyWhen(
+      //   path.address,
+      //   ({ valueOf }) => valueOf(path.address.country) === 'CA',
+      //   caZipSchema,
+      // );
+      // applyWhenValue(
+      //   path.address,
+      //   (address): address is UsAddress => address.country === 'US',
+      //   (address) => pattern(address.zip, /^\d{5}$/, { message: 'Zip code must be 5 digits' }),
+      // );
+      // applyWhenValue(
+      //   path.address,
+      //   (address): address is CaAddress => address.country === 'CA',
+      //   (address) =>
+      //     pattern(
+      //       address.zip,
+      //       /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ] \d[ABCEGHJKLMNPRSTVWXYZ]\d$/,
+      //       {
+      //         message: 'Postal code must follow the A1A 1A1 format',
+      //       },
+      //     ),
+      // );
+      // debounce(path.address.zip, 300);
+      // debounce(path.address.zip, 'blur');
+      registerZipValidation(path.address.zip, path.address.country);
+    },
+    {
+      submission: {
+        action: async (field) => {
+          try {
+            await this.userService.saveUserInfo(field().value());
+            field().reset({
+              ...this.INITIAL_USER_INFO,
+            });
+            return;
+          } catch {
+            return { kind: 'serverError', message: 'Failed to save user info' };
+          }
+        },
+        onInvalid: () => {
+          console.log('Invalid');
+        },
+      },
+    },
+  );
+
+  async onSave() {
+    const success = await submit(this.userForm, async (field) => {
+      try {
+        await this.userService.saveUserInfo(field().value());
+        field().reset({
+          ...this.INITIAL_USER_INFO,
+        });
+        return;
+      } catch {
+        return { kind: 'serverError', message: 'Failed to save user info' };
+      }
+    });
+    if (success) {
+      console.error('Successfully saved user info');
+    }
+  }
+}
